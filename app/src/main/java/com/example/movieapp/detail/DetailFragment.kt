4b31876ac203f910f279f98656db40c9e3532cb0
Marvin Moosbacher <moosbacher94@gmail.com>
2021-02-19 12:21:22 +0100

    Migrated detailsScreen layout to jetpack compose

--- app/src/main/java/com/example/movieapp/detail/DetailFragment.kt
+++ app/src/main/java/com/example/movieapp/detail/DetailFragment.kt
@@ -5,37 +5,87 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.movieapp.R
-import com.example.movieapp.Utils
-import com.example.movieapp.databinding.FragmentDetailBinding
+import com.example.movieapp.models.MovieProperty
+import com.example.movieapp.utils.DEFAULT_RECIPE_IMAGE
+import com.example.movieapp.utils.MOVIE_BASE_URL
+import com.example.movieapp.utils.loadPicture
 
-class DetailFragment : Fragment(R.layout.fragment_detail) {
 
-    private lateinit var binding: FragmentDetailBinding
+class DetailFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        binding = FragmentDetailBinding.inflate(inflater)
+        return ComposeView(requireContext()).apply {
 
-        init()
+            val args = DetailFragmentArgs.fromBundle(requireArguments())
 
-        return binding.root
+            setContent {
+                MaterialTheme {
+                    Scaffold(backgroundColor = Color.Black, contentColor = Color.White) {
+                        DetailsScreenContent(args.movie)
+                    }
+                }
+            }
+        }
     }
+}
 
+@Composable
+fun DetailsScreenContent(movie: MovieProperty) {
 
-    private fun init() {
-        val args = DetailFragmentArgs.fromBundle(requireArguments())
-        Utils.bindMovieImage(args.movie.movieImg, binding.movieImageView)
+    val movieUrl = MOVIE_BASE_URL + movie.movieImg
 
-        binding.detailTitle.text = args.movie.title
-        binding.detailVoteAverage.text =
-            context?.getString(R.string.voteAverage, args.movie.voteAverage.toString())
-        binding.detailReleaseDate.text = args.movie.releaseDate
-        binding.overview.text = args.movie.overview
+    val image =
+        loadPicture(url = movieUrl, defaultImage = DEFAULT_RECIPE_IMAGE).value
 
+    LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
+        item {
+            image?.let {
+                Image(
+                    bitmap = it.asImageBitmap(),
+                    contentDescription = "MovieImg",
+                    modifier = Modifier.width(220.dp).padding(top = 30.dp, bottom = 40.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Text(
+                text = movie.title,
+                style = TextStyle(fontSize = 20.sp, textAlign = TextAlign.Center)
+            )
+            Text(
+                text = LocalContext.current.getString(
+                    R.string.voteAverage,
+                    movie.voteAverage.toString()
+                ),
+                modifier = Modifier.padding(10.dp)
+            )
+            Text(text = movie.releaseDate)
+            Text(
+                text = movie.overview,
+                style = TextStyle(textAlign = TextAlign.Center),
+                modifier = Modifier.padding(30.dp)
+            )
+        }
     }
 }
